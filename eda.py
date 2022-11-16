9026e5f15cf351d58bcb89b001392bc330a0751d
Warren Seeds <65046852+seedsw23@users.noreply.github.com>
2022-11-15 20:27:41 -0500

    Added violin plots

--- eda.py
+++ eda.py
@@ -8,6 +8,7 @@ import numpy as np
 from scipy.io.arff import loadarff
 from mlxtend.plotting import scatterplotmatrix, heatmap
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 print('\n==================Exploratory Data Analysis==================\n')
 
@@ -79,6 +80,18 @@ col2 = ['Attr17', 'Attr18','Attr19','Attr20','Attr21','Attr22','Attr23','Attr24'
 col3 = ['Attr33', 'Attr34','Attr35','Attr36','Attr37','Attr38','Attr39','Attr40','Attr41','Attr42','Attr43','Attr44','Attr45','Attr46','Attr47','Attr48','class']
 col4 = ['Attr49', 'Attr50','Attr51','Attr52','Attr53','Attr54','Attr55','Attr56','Attr57','Attr58','Attr59','Attr60','Attr61','Attr62','Attr63','Attr64','class']
 
+
+# Violin plot
+features = df.iloc[:,0:63]
+plt.figure(figsize=(10,40))
+j = 0
+for i in features:
+    plt.subplot(21,3,j+1)
+    sns.violinplot(x=df["class"],y=df[i],palette=["red","green"])
+    plt.title(i)
+    plt.axhline(df[i].mean(),linestyle = "dashed", label ="Mean value = " + str(round(df[i].mean(), 2)))
+    plt.legend(loc="best")
+    j = j + 1
 
 
 ''' can not do Pearson's R until we deal with null values'''
